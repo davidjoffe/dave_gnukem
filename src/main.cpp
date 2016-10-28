47904e5a979b16733672ec8b969354fdc9003a74
davidjoffe <david.joffe@tshwanedje.com>
2016-10-29 00:52:18 +0200

    Version 0.66 [Windows only] release

--- src/main.cpp
+++ src/main.cpp
@@ -358,7 +358,7 @@ void DoMainMenu()
 		if (g_pImgMain)
 			djgDrawImage( pVisBack, g_pImgMain, 0, 0, g_pImgMain->Width(), g_pImgMain->Height() );
 		char sz[100]={0};
-		sprintf(sz,"%s","v0.65b (23 Oct 2016)");
+		sprintf(sz,"%s","v0.66 (29 Oct 2016)");
 		GraphDrawString(pVisBack, g_pFont8x8, 320 - strlen(sz)*8, 200 - 8*2, (unsigned char*)sz);
 		sprintf(sz,"%s","http://djoffe.com/");
 		GraphDrawString(pVisBack, g_pFont8x8, 320 - strlen(sz)*8, 200 - 8, (unsigned char*)sz);
