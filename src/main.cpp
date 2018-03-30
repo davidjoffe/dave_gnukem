09998c239ddfd1708095fab658eb15ae6feeab87
davidjoffe <david.joffe@tshwanedje.com>
2018-03-30 23:55:40 +0200

    Minor layout changes of version info etc. on main menu

--- src/main.cpp
+++ src/main.cpp
@@ -394,8 +394,9 @@ void DoMainMenu()
 		if (g_pImgMain)
 			djgDrawImage( pVisBack, g_pImgMain, 0, 0, g_pImgMain->Width(), g_pImgMain->Height() );
 		char sz[100]={0};
-		sprintf(sz,"%s","v0.95 (30 Mar 2018)");
-		GraphDrawString(pVisBack, g_pFont8x8, 320 - strlen(sz)*8, 200 - 8*2, (unsigned char*)sz);
+		sprintf(sz,"%s","v0.95");// (30 Mar 2018)");
+		//GraphDrawString(pVisBack, g_pFont8x8, 320 - strlen(sz)*8, 200 - 8*2, (unsigned char*)sz);
+		GraphDrawString(pVisBack, g_pFont8x8, 0, 200 - 8, (unsigned char*)sz);
 		sprintf(sz,"%s","djoffe.com");
 		GraphDrawString(pVisBack, g_pFont8x8, 320 - strlen(sz)*8, 200 - 8, (unsigned char*)sz);
 
