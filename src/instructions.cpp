dd26ed23bda6ea99803d144bf031f480fdfc696d
davidjoffe <david.joffe@tshwanedje.com>
2018-03-30 22:18:55 +0200

    Add 'About' page to main menu; add menu border drop-shadows; add drop-shadow effect to font used in About and Instructions auto-type-effect text

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -22,6 +22,7 @@ License: GNU GPL Version 2
 #include"djinput.h"//GetKeyString()
 
 const std::string sPLOT("STORY: The year is $CURRENTYEAR+8. An evil genius, Dr Proetton, has been hired by the CIA to infect the world's computers with a virus called SystemD, crippling them. Only you can stop him. You must find the floppy disk with the Devuan Antivirus on it, and install it on the master computer, which is hidden in Vault7.\n*Any resemblance to actual persons or entities is purely coincidental");
+const std::string sABOUT("Dave Gnukem is an open source retro-style 2D scrolling platform shooter similar to, and inspired by, Duke Nukem 1 (a famous 1991 game). It has no affiliation with the original game; this is a \"spare-time\" project created by David Joffe, with contributions by many others. It includes a level editor and cross-platform support.\n\nThe original Duke Nukem 1 had 16-color EGA 320x200 graphics; the aim here is similar but different gameplay and 'look and feel'.\n\nSOURCE: github.com/davidjoffe/dave_gnukem\n\n### ABOUT DUKE NUKEM 1 ###\nDuke Nukem 1 was a famous original 16-color 320x200 'classic' game released by Apogee Software in 1991 that launched the Duke Nukem series. The original Duke Nukem 1 was created by Todd Replogle (co-creator of the Duke Nukem series), John Carmack (of id Software), Scott Miller (founder of 3D Realms), Allen H. Blum III, George Broussard, and Jim Norwood.");
 
 std::string GetKeyStringS(int nSDLKeyCode)
 {
@@ -88,72 +89,47 @@ std::string PadSpaces(const std::string& sStr, unsigned int uLen)
 	return "";
 }
 //-------------------------------------------------------
-void ShowInstructions()
+void DoShow(const std::string& sLinesOrigText, const std::vector<std::string>& asText,float fApproxFramerateTarget=100.f)
 {
 	unsigned int FW=8;
 	unsigned int FH=8;
-	std::string sALPHABET=
-		" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}";
+	std::string sALPHABET=" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}";
 	djImage* pImgFont = NULL;
-	if (pImgFont==NULL)
+	djImage* pImgFontS = NULL;//shadow
+	pImgFont = new djImage;
+	if (pImgFont->Load( "data/fonts/simple_6x8.tga" )>=0)
 	{
-		pImgFont = new djImage;
-		if (pImgFont->Load( "data/fonts/simple_6x8.tga" )>=0)
-		{
-			djCreateImageHWSurface( pImgFont );
-			FW=6;
-			FH=8;
-		}
-		else
-		{
-			//djDestroyImageHWSurface( pImgFont );
-			djDEL(pImgFont);
-		}
+		djCreateImageHWSurface( pImgFont );
+		FW=6;
+		FH=8;
+	}
+	else
+	{
+		djDEL(pImgFont);
+	}
+	if (!pImgFont)return;
+	pImgFontS = new djImage;
+	if (pImgFontS->Load( "data/fonts/simple_6x8_shadow.tga" )>=0)
+	{
+		djCreateImageHWSurface( pImgFontS );
+	}
+	else
+	{
+		djDEL(pImgFontS);
 	}
 	//CBSU[could be sped up'- fixFUtureMaybe]: Pre-build "map" of characters to their indexes in the fontsprite: std::map<char,unsigned int> mapFontChars;
 
-	unsigned int uWIDTH_STORY = (320 - 16) / FW;
-	unsigned int uWIDTH = (320 - 16) / 8;
+	unsigned int uWIDTH_NUMCOLS = (320 - 16) / FW;
 
-	std::vector<std::string> asStory;
-	WrapString(sPLOT, uWIDTH_STORY, asStory);
-
-	std::vector<std::string> asText;
-	//asText.push_back("012345678901234567890123456789012345678901234567890123456789");
-	asText.push_back("");
-	asText.push_back("INSTRUCTIONS");
-	WrapString("Find the exit in each level, while dodging or shooting monsters.", uWIDTH, asText);
-
-	asText.push_back("");
-	asText.push_back("KEYS:");
-
-	char szBuf[1024] = {0};
-	sprintf(szBuf, "%s", GetKeyString(g_anKeys[KEY_LEFT]));
-	asText.push_back(std::string("LEFT:  ") + szBuf
-		+ PadSpaces(szBuf,18-7) + std::string("SHOOT: ") + GetKeyStringS(g_anKeys[KEY_SHOOT]) );
-	sprintf(szBuf, "%s", GetKeyString(g_anKeys[KEY_RIGHT]));
-	asText.push_back(std::string("RIGHT: ") + szBuf
-		+ PadSpaces(szBuf,18-7) + std::string("JUMP:  ") + GetKeyStringS(g_anKeys[KEY_JUMP]) );
-	sprintf(szBuf, "%s", GetKeyString(g_anKeys[KEY_ACTION]));
-	//asText.push_back(std::string(" Action: ") + szBuf + std::string(" (Activate doors,"));
-	//asText.push_back("  exits, lifts, teleporters etc.)");
-	asText.push_back(std::string("ACTION:") + szBuf + std::string(" Activate doors, lifts, etc."));
-	
-	// Hrm, these may be platform specific, fixmeLOW:
-	asText.push_back("PGUP/DN: Volume   INS: Toggle sounds");
-	asText.push_back("ESC: In-game menu: Save/Restore/Quit");
-	//asText.push_back("F4,F5: Sprite/Level editors");
-	//asText.push_back("Use the action key to open doors, activate lifts, teleporters, etc.");
-	asText.push_back("SECRET BONUSES:");
-	asText.push_back("-Shoot all security cameras in level");
-	asText.push_back("-Collect letters G,N,U,K,E,M in order");
+	std::vector<std::string> asLines;
+	WrapString(sLinesOrigText, uWIDTH_NUMCOLS, asLines);
 
 	// Note here some of the units are in pixels, others in '8-pixel-block' units
 
 	const int X = 4;//Pixel offset left (top-left corner)
 	const int Y = 4;//Pixel offset top (top-left corner)
 	const int H = (int)asText.size() + 1
-		+ (int)asStory.size()
+		+ (int)asLines.size()
 		;
 	int W = (320/8)-1;//5;
 
@@ -183,7 +159,7 @@ void ShowInstructions()
 		Y,
 		W*8,
 		1
-		);
+	);
 	//bottom+right 'dark' lines
 	djgSetColorFore(pVisBack,djColor(35,35,35));
 	djgDrawRectangle( pVisBack,
@@ -197,19 +173,19 @@ void ShowInstructions()
 		Y+2,
 		1,
 		H*8-1
-		);
+	);
 
 	// Draw lines of text
 	//GraphDrawString( pVisBack, g_pFont8x8, X+16, 0, (const unsigned char*)"INSTRUCTIONS");
 	for ( int i=0; i<(int)asText.size(); ++i )
 	{
 		GraphDrawString( pVisBack, g_pFont8x8, X+4, (Y+4)+i*8
-			+ asStory.size()*8+4
+			+ asLines.size()*8+4
 			, (unsigned char*)asText[i].c_str() );
 	}
 
-	// Aim for 100fps
-	const float fTimeFrame = (1.0f / 100.0f);
+	// Aim (approximately) for some given framerate, which will also determine the text effect speed
+	const float fTimeFrame = (1.0f / fApproxFramerateTarget);
 
 	// Start out by being at next time
 	float fTimeNext = djTimeGetTime();
@@ -227,7 +203,7 @@ void ShowInstructions()
 	{
 		fTimeNow = djTimeGetTime();
 		fTimeNext = fTimeNow + fTimeFrame;
-		
+
 		// Sleep a little to not hog CPU to cap menu update (frame rate) at approx 10Hz
 		while (fTimeNow<fTimeNext)
 		{
@@ -239,32 +215,31 @@ void ShowInstructions()
 		do
 		{
 			// Should add some randomness, but [low] this is 'OK' for now
-			if (uTypingCurLine<asStory.size())
+			if (uTypingCurLine<asLines.size())
 			{
-				if (uTypingCurStrPos>=asStory[uTypingCurLine].length())
+				if (uTypingCurStrPos>=asLines[uTypingCurLine].length())
 				{
 					// NEXT LINE
 					++uTypingCurLine;
 					uTypingCurStrPos = 0;
-					if (uTypingCurLine>=asStory.size())
+					if (uTypingCurLine>=asLines.size())
 						bTypingFinished = true;
 				}
 				else
 				{
 					// future? add a 'cursor'
-					//const unsigned char szBACKGROUND[2]={255-2,0};
-					//GraphDrawString( pVisBack, g_pFont8x8, X+4+uTypingCurStrPos*8  , (Y+4)+uTypingCurLine*8, szBACKGROUND );
-					//const unsigned char szCURSOR[2]={255-1,0};
-					//GraphDrawString( pVisBack, g_pFont8x8, X+4+uTypingCurStrPos*8+8, (Y+4)+uTypingCurLine*8, szCURSOR );
-					
 					if (pImgFont)
 					{
 						// Find character index using alphabet
-						char c = asStory[uTypingCurLine][uTypingCurStrPos];
+						char c = asLines[uTypingCurLine][uTypingCurStrPos];
 						int nAlphPos = (int)sALPHABET.find(c);
 						if (nAlphPos>=0)
 						{
-							// Blit from font
+							// Blit from font (first dropshadow version, if available)
+							if (pImgFontS) djgDrawImageAlpha( pVisBack, pImgFontS,
+								FW*nAlphPos,0,
+								X + 4 + uTypingCurStrPos*FW + 1, Y + 4 + uTypingCurLine*FH + 1,
+								FW,FH);
 							djgDrawImageAlpha( pVisBack, pImgFont,
 								FW*nAlphPos,0,
 								X + 4 + uTypingCurStrPos*FW, Y + 4 + uTypingCurLine*FH,
@@ -278,7 +253,7 @@ void ShowInstructions()
 						// Need NULL-separated string that's one character. So, two chars,
 						// first is the character, and second is a 0 to terminate string.
 						unsigned char szCurrentCharacter[2]={0};
-						szCurrentCharacter[0] = asStory[uTypingCurLine][uTypingCurStrPos];
+						szCurrentCharacter[0] = asLines[uTypingCurLine][uTypingCurStrPos];
 						GraphDrawString( pVisBack, g_pFont8x8,
 							X+4+uTypingCurStrPos*8, (Y+4)+uTypingCurLine*8,
 							szCurrentCharacter );
@@ -306,7 +281,7 @@ void ShowInstructions()
 			case SDL_KEYDOWN:
 				if (!bTypingFinished &&
 					(Event.key.keysym.sym==SDLK_SPACE
-					|| Event.key.keysym.sym==SDLK_RETURN)
+						|| Event.key.keysym.sym==SDLK_RETURN)
 					)
 				{
 					bFinishTyping = true;//Insta-finish typing
@@ -337,6 +312,49 @@ void ShowInstructions()
 		djDestroyImageHWSurface( pImgFont );
 		djDEL(pImgFont);
 	}
+	if (pImgFontS!=NULL)
+	{
+		djDestroyImageHWSurface( pImgFontS );
+		djDEL(pImgFontS);
+	}
+}
+void ShowInstructions()
+{
+	std::vector<std::string> asText;
+	asText.push_back("");
+	asText.push_back("INSTRUCTIONS");
+	unsigned int uWIDTH = (320 - 16) / 8;
+	WrapString("Find the exit in each level, while dodging or shooting monsters.", uWIDTH, asText);
 
-	return;
+	asText.push_back("");
+	asText.push_back("KEYS:");
+
+	char szBuf[1024] = {0};
+	sprintf(szBuf, "%s", GetKeyString(g_anKeys[KEY_LEFT]));
+	asText.push_back(std::string("LEFT:  ") + szBuf
+		+ PadSpaces(szBuf,18-7) + std::string("SHOOT: ") + GetKeyStringS(g_anKeys[KEY_SHOOT]) );
+	sprintf(szBuf, "%s", GetKeyString(g_anKeys[KEY_RIGHT]));
+	asText.push_back(std::string("RIGHT: ") + szBuf
+		+ PadSpaces(szBuf,18-7) + std::string("JUMP:  ") + GetKeyStringS(g_anKeys[KEY_JUMP]) );
+	sprintf(szBuf, "%s", GetKeyString(g_anKeys[KEY_ACTION]));
+	//asText.push_back(std::string(" Action: ") + szBuf + std::string(" (Activate doors,"));
+	//asText.push_back("  exits, lifts, teleporters etc.)");
+	asText.push_back(std::string("ACTION:") + szBuf + std::string(" Activate doors, lifts, etc."));
+
+	// Hrm, these may be platform specific, fixmeLOW:
+	asText.push_back("PGUP/DN: Volume   INS: Toggle sounds");
+	asText.push_back("ESC: In-game menu: Save/Restore/Quit");
+	//asText.push_back("F4,F5: Sprite/Level editors");
+	//asText.push_back("Use the action key to open doors, activate lifts, teleporters, etc.");
+	asText.push_back("SECRET BONUSES:");
+	asText.push_back("-Shoot all security cameras in level");
+	asText.push_back("-Collect letters G,N,U,K,E,M in order");
+
+	DoShow(sPLOT, asText);
+}
+
+void ShowAbout()
+{
+	std::vector<std::string> asText;//<- Empty for the about page.
+	DoShow(sABOUT,asText,200.f);
 }
