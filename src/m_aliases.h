a6fa164280b7a7218e42ae910dbe450f002dc1fc
davidjoffe <david@djoffe.com>
2022-01-17 18:26:09 +0200

    Removing unused aliases as some e.g. byte were unused anyway in our codebase and cause conflicts with e.g. std::byte on newer c++ versions e.g. c++17

--- src/m_aliases.h
+++ src/m_aliases.h
@@ -8,6 +8,7 @@
  * Desciption: a definition of few aliasing typenames. Removed from
  *		sys_defs.h by request of YvL.
  *
+ * dj2022-01 Removing unused aliases as some e.g. byte were unused anyway in our codebase and cause conflicts with e.g. std::byte on newer c++ versions e.g. c++17 .. if we need similar things again we can create new ones and/or rather use the newer c++ types
  */
 
 
@@ -16,16 +17,7 @@
 #define ALIASES_H__KRANKLYS__
 
 
-#define byte	unsigned char
-#define sbyte	signed char
-#define word	unsigned short int
-#define sword	signed short int
 #define dword	unsigned long int
-#define sdword	signed long int
-#define qword	unsigned long long int
-#define sqword	signed long long int
-#define bbool	unsigned char
-#define dwbool	unsigned long int
 
 
 #endif		// #ifndef ALIASES_H__KRANKLYS__
