0ce94dab94fc395088b7ef16f7eb7187f2b41772
David Joffe <david@djoffe.com>
2022-11-24 01:34:46 +0200

    Minor header refactor

--- src/djgamelib.h
+++ src/djgamelib.h
@@ -1,5 +1,6 @@
 /*--------------------------------------------------------------------------*/
 // This is just here for convenience
+// [dj2022-11 not a fan of this type of convenience helper anymore as over years can lead to slow compiles as projects get larger etc. .. only include what you need when you need it]
 /*--------------------------------------------------------------------------*/
 #include "djgraph.h"
 #include "djimage.h"
