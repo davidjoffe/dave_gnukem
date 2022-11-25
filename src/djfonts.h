c61b3460562d63e8b6532c5c55c6da61df718f10
David Joffe <david@djoffe.com>
2022-11-25 21:45:00 +0200

    New cpp/h file for new vector-based and Unicode font stuff

---
+++ src/djfonts.h
@@ -0,0 +1,12 @@
+//Copyright (C) 2022 David Joffe / Dave Gnukem project
+//
+//dj2022-11-25 new vector-based font stuff (TTF/OTF) and Unicode font helper stuff.
+//NB Design-wise this is conceptually part of the generic reusable code so shouldn't have dependencies to any Dave Gnukem specific parts of the codebase
+
+#ifndef _DJFONTS_H_
+#define _DJFONTS_H_
+
+#include "config.h"
+
+
+#endif
