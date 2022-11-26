54adce3afcd21d2987d020c9aaf0a18b4b51b900
David Joffe <david@djoffe.com>
2022-11-26 23:43:35 +0200

    New djfile.h/cpp for refactoring some file stuff and adding some helpers

---
+++ src/djfile.cpp
@@ -0,0 +1,9 @@
+//Copyright (C) 1995-2022 David Joffe / Dave Gnukem project
+//
+//dj2022-11 refactoring some file stuff into new djfile.h/cpp and maybe adding some more file- and path-related helpers and maybe some
+
+/*--------------------------------------------------------------------------*/
+#include "config.h"
+#include "djfile.h"
+
+
