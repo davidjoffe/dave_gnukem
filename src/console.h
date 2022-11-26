460c6f5b32a70ff8bc0a5ef2bea796ab1c843005
David Joffe <david@djoffe.com>
2022-11-26 18:03:52 +0200

    Refactoring new src files for console ingame message stuff, and remove unnecessary over-complicating data files from Visual Studio project files

---
+++ src/console.h
@@ -0,0 +1,12 @@
+//Copyright (C) 1995-2022 David Joffe / Dave Gnukem project
+//
+//dj2022-11 just refactoring crude simple console message (or ingame message) stuff into own .h/cpp
+//[low prio thoughts]: Not quite sure if this is a 'console' or a 'game message' and maybe in future there might be two distint things (in-game messages vs an actual console where e.g. one might type commands say)
+
+/*--------------------------------------------------------------------------*/
+#ifndef _dj_CONSOLE_H_
+#define _dj_CONSOLE_H_
+
+#include "config.h"
+
+#endif
