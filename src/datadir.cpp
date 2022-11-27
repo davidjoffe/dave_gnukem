12412fc09eb46edbff7ba8ba55a8514491fbb943
David Joffe <david@djoffe.com>
2022-11-27 04:50:59 +0200

    Adding datadir.cpp and new helpers djDataDir() etc. to make DATA_DIR stuff more flexible (e.g. make it easier either for porters to override, or add functionality like checking fallback folders - e.g. check /usr/share but if not found e.g. check relative say to the executable path)

---
+++ src/datadir.cpp
@@ -0,0 +1,30 @@
+//Copyright (C) 1995-2022 David Joffe
+//
+
+//dj2022-11 adding datadir.cpp to corresponding to old datadir.h to extend this functionality
+//dj2022-11 Adding new helper djDataDir() (and corresponding set) to make this more flexible (e.g. make it easier either for porters to override, or add functionality like checking fallback folders - e.g. check /usr/share but if not found e.g. check relative say to the executable path)
+
+#include "config.h"
+#include "datadir.h"
+
+#include <string>
+
+// [dj2022-11] Slightly experimental but the idea here is to set the default g_sDataDir based on the DATA_DIR (which e.g. comes from Makefile) but this allows dynamic overriding (or selecting fallbacks)
+// NOTE we don't currently 'expose' this g_sDataDir (for now?) as it's sort of the 'backend' - use the get/set functions below
+std::string g_sDataDir = DATA_DIR;
+
+const char* djDataDir()
+{
+	//if (g_sDataDir.empty())
+	//	g_sDataDir = DATA_DIR;
+
+	return g_sDataDir.c_str();
+}
+
+void djSetDataDir(const char* szDataDir)
+{
+	if (szDataDir==nullptr)
+		g_sDataDir.clear();
+	else
+		g_sDataDir = szDataDir;
+}
--- src/datadir.h
+++ src/datadir.h
@@ -3,15 +3,23 @@
 \brief   Data directory
 \author  David Joffe
 
-Copyright (C) 1999-2018 David Joffe
+Copyright (C) 1999-2022 David Joffe
 */
+//dj2022-11 adding datadir.cpp to corresponding to old datadir.h to extend this functionality
+//dj2022-11 Adding new helper djDataDir() (and corresponding set) to make this more flexible (e.g. make it easier either for porters to override, or add functionality like checking fallback folders - e.g. check /usr/share but if not found e.g. check relative say to the executable path)
 
-#ifndef _DATADIR_H_
-#define _DATADIR_H_
+#ifndef _dj_DATADIR_H_
+#define _dj_DATADIR_H_
 
 //! Data directory [dj2017-08 should this move to config.h?]
 #ifndef DATA_DIR
 #define DATA_DIR "data/"
 #endif
+
+//[dj2022-11]
+extern const char* djDataDir();
+
+//[dj2022-11]
+extern void djSetDataDir(const char* szDataDir);
 
 #endif
