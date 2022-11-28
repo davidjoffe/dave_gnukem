b7548350437afab4cd7ad72d1d67c75f247251d9
David Joffe <david@djoffe.com>
2022-11-28 22:46:14 +0200

    Refactor new slightly beta viewport background shadow effect into new .h/cpp

---
+++ src/effect_viewportshadow.cpp
@@ -0,0 +1,10 @@
+// effect_viewportshadow.cpp
+// Copyright (C) 1995-2022 David Joffe / Dave Gnukem project
+//
+// dj2022-11 just refactoring this recent new effect from game.cpp into separate .h/cpp files for neatness as game.cpp getting a bit long and cluttered
+//
+
+/*--------------------------------------------------------------------------*/
+#include "config.h"
+#include "effect_viewportshadow.h"
+
---
+++ src/effect_viewportshadow.h
@@ -0,0 +1,13 @@
+// effect_viewportshadow.h
+// Copyright (C) 1995-2022 David Joffe / Dave Gnukem project
+//
+// dj2022-11 just refactoring this recent new effect from game.cpp into separate .h/cpp files for neatness as game.cpp getting a bit long and cluttered
+//
+
+/*--------------------------------------------------------------------------*/
+#ifndef _dj_EFFECT_VIEWPORTSHADOW_H_
+#define _dj_EFFECT_VIEWPORTSHADOW_H_
+
+#include "config.h"
+
+#endif
