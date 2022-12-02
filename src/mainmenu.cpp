09a461db99ce1f124188e83e525dcf2bc66d13af
davidjoffe <david@djoffe.com>
2022-12-02 21:14:06 +0200

    Refactoring old menu code into new mainmenu.h/cpp files for neatness and toward localization

---
+++ src/mainmenu.cpp
@@ -0,0 +1,9 @@
+//Copyright (C) 1995-2022 David Joffe
+//
+// dj2022-12 Just refactoring old main menu code out of main.cpp (and in prep toward some localization stuff)
+
+/*--------------------------------------------------------------------------*/
+#include "config.h"
+#include "mainmenu.h"
+
+
---
+++ src/mainmenu.h
@@ -0,0 +1,12 @@
+//Copyright (C) 1995-2022 David Joffe
+//
+// dj2022-12 Just refactoring old main menu code out of main.cpp (and in prep toward some localization stuff)
+
+/*--------------------------------------------------------------------------*/
+#ifndef _djMAINMENU_H_
+#define _djMAINMENU_H_
+
+#include "config.h"
+
+
+#endif//_djMAINMENU_H_
