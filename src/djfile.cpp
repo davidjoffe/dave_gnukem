1d293791e080c4edf8738bfc6417409316f2c0ee
davidjoffe <david@djoffe.com>
2023-12-21 13:08:07 +0200

    Copyright year

--- src/djfile.cpp
+++ src/djfile.cpp
@@ -1,4 +1,4 @@
-//Copyright (C) 1995-2022 David Joffe / Dave Gnukem project
+//Copyright (C) 1995-2023 David Joffe / Dave Gnukem project
 //
 //dj2022-11 refactoring some file stuff into new djfile.h/cpp and maybe adding some more file- and path-related helpers and maybe some
 
--- src/djfonts.h
+++ src/djfonts.h
@@ -1,4 +1,4 @@
-//Copyright (C) 2022 David Joffe / Dave Gnukem project
+//Copyright (C) 2022-2023 David Joffe / Dave Gnukem project
 //
 //dj2022-11-25 new vector-based font stuff (TTF/OTF) and Unicode font helper stuff.
 //NB Design-wise this is conceptually part of the generic reusable code so shouldn't have dependencies to any Dave Gnukem specific parts of the codebase
