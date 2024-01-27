2738f31a35c1411861f5b41662f544544a959132
davidjoffe <david@djoffe.com>
2024-01-27 16:36:43 +0200

    ignore

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 # This script is not important to the core source and it is suggested to avoid building anything based on this script as it may change or move etc. in future - it's just to save time for dev
 
 make clean && make -j8
+
+# Still thinking about this DdjUNICODE_SUPPORT etc. dj2024:
+#make clean && make -j8 CXXFLAGS="-DdjUNICODE_SUPPORT"
