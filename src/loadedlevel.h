69c84aa4f92ffa40ff020d7972a0e8f2b0634aa8
davidjoffe <david@djoffe.com>
2024-02-10 18:45:09 +0200

    New collision rects

--- src/loadedlevel.h
+++ src/loadedlevel.h
@@ -11,6 +11,8 @@ Copyright (C) 1995-2024 David Joffe
 #ifndef _LOADEDLEVEL_H_
 #define _LOADEDLEVEL_H_
 
+#include <vector>
+#include "djrect.h" // Include the rectangle template class
 //#include "config.h"
 
 // Forward declarations (don't include actual headers here for compile speed reasons etc.)
@@ -24,6 +26,10 @@ struct tsLoadedLevel
 	// Main game backing sprite (while playing) - new [dj2023-11]
 	djSprite* pBack1 = nullptr;
 	djSprite* Back1() const { return pBack1; }
+
+	// Vector of rectangles (specific case of polygons, but slightly faster collision detection if we do it this way) for collision detection [dj2024]
+	std::vector<djRectD> collisionRects;
+	//std::vector<djRectD> collisionPolys;
 
 	// For speed reasons an extra copy of pBack1->GetImage() (if loaded) or nullptr if not
 	djImage* pImgBack1 = nullptr;
