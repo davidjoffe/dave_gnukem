c12e76a36a173be23d06545b106cd33b983af1ec
David Joffe <david@djoffe.com>
2024-10-16 10:10:17 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # Very small build helper script
 # This script is not important to the core source and it is suggested to avoid building anything based on this script as it may change or move etc. in future - it's just to save time for dev
 
+@echo Make sure you have first installed dependencies if necessary, e.g. "sudo apt install libsdl2-dev", "sudo apt install libsdl2-mixer-dev"
+
 make clean && make -j8
 
 # Still thinking about this DdjUNICODE_SUPPORT etc. dj2024:
